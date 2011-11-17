6162ddb5eb60176c3f9d4b9659c6a41718dcc8d2
iDimka <dmsa@nb-dmsa.local>
2011-11-17 11:05:41 +0300

    import

---
+++ cruise/cruise/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  cruise
+//
+//  Created by Dmitry Sazanovich on 17/11/2011.
+//  Copyright (c) 2011 iDimka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
---
+++ cruise/cruiseTests/cruiseTests.m
@@ -0,0 +1,32 @@
+//
+//  cruiseTests.m
+//  cruiseTests
+//
+//  Created by Dmitry Sazanovich on 17/11/2011.
+//  Copyright (c) 2011 iDimka. All rights reserved.
+//
+
+#import "cruiseTests.h"
+
+@implementation cruiseTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in cruiseTests");
+}
+
+@end
